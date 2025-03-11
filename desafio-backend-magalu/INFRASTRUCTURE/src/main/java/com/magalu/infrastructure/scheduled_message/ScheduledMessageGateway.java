bfa65ef76baac221036399f576b0a4bc13859696
alexandrelsantana <alexandre.lsantana@outlook.com>
2025-03-11 18:00:15 -0300

    ScheduledMessageGateway implements

--- desafio-backend-magalu/INFRASTRUCTURE/src/main/java/com/magalu/infrastructure/scheduled_message/ScheduledMessageGateway.java
+++ desafio-backend-magalu/INFRASTRUCTURE/src/main/java/com/magalu/infrastructure/scheduled_message/ScheduledMessageGateway.java
@@ -2,17 +2,35 @@ package com.magalu.infrastructure.scheduled_message;
 
 import com.magalu.domain.entity.scheduled_message.ScheduledMessage;
 import com.magalu.domain.entity.scheduled_message.ScheduledMessageGatewayInterface;
+import com.magalu.infrastructure.persistence.ScheduledMessageJpaEntity;
+import com.magalu.infrastructure.persistence.ScheduledMessageRepository;
+import com.magalu.infrastructure.mappers.SchedulerMessageMapper;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 public class ScheduledMessageGateway implements ScheduledMessageGatewayInterface {
+
+    private final ScheduledMessageRepository scheduledMessageRepository;
+
+    public ScheduledMessageGateway(ScheduledMessageRepository scheduledMessageRepository) {
+        this.scheduledMessageRepository = scheduledMessageRepository;
+    }
+
     @Override
     public ScheduledMessage findById(String id) {
-        return null;
+        Optional<ScheduledMessageJpaEntity> byId = this.scheduledMessageRepository.findById(id);
+
+        if (byId.isEmpty()){
+            return null;
+        }
+
+        return SchedulerMessageMapper.toDomain(byId.get());
     }
 
     @Override
     public void save(ScheduledMessage scheduledMessage) {
-
+        this.scheduledMessageRepository.save(SchedulerMessageMapper.fromDomain(scheduledMessage));
     }
 }
